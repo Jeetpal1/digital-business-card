26622b7c63919a5919ce9668fa6e836599d9b859
Jeetpal <singhjeetpal99@gmail.com>
2022-05-01 16:47:08 -0500

    added link to my portfolio website to my my pic

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,22 @@ import linkedinButton from "../images/linkedin-button.png";
 function Header() {
   return (
     <div className="info">
-      <img src={myPic} className="my-pic" />
+      <a href="https://jeetpal-singh.web.app/">
+        <img src={myPic} className="my-pic" />
+      </a>
 
       <h2 className="name">Jeetpal Singh</h2>
       <h5 className="job-title">Software Developer</h5>
       <a href="https://jeetpal-singh.web.app/">jeetpal.website</a>
       <div className="buttons">
         <a href="mailto:singhjeetpal001@gmail.com" target="_blank">
-          <img src={linkedinButton} alt="Email button" className="button" />
+          <img src={emailButton} alt="Email button" className="button" />
         </a>
         <a
           href="https://www.linkedin.com/in/jeetpal-singh-8630a61aa/"
           target="_blank"
         >
-          <img src={emailButton} alt="Linkedin button" className="button" />
+          <img src={linkedinButton} alt="Linkedin button" className="button" />
         </a>
       </div>
     </div>
